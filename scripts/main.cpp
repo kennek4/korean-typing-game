a0d478a21ea97fe4781356b9b7e8b55cfdc3222b
kennek4 <kentabanay@gmail.com>
2025-02-19 14:57:27 -0700

    refactor: changed include statement position

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,15 +1,10 @@
+#include "SDL3_image/SDL_image.h"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_error.h>
 #include <SDL3/SDL_events.h>
-#include <SDL3/SDL_filesystem.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_oldnames.h>
-#include <SDL3/SDL_render.h>
-#include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_video.h>
-#include <SDL3_image/SDL_image.h>
-#include <cstddef>
-#include <cstring>
 #include <iostream>
 #include <ostream>
 
